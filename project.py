2a4430172c1d2c5552cc33e9b9ea4c32b95e9f1a
burak <rburaksaritas@gmail.com>
2022-12-20 02:42:25 +0300

    Add features for contitonal probability
    Add features compute_unigrams_bigrams, compute_conditional_probability
    Fix: merge_data_master, merge_data_workers.

--- project.py
+++ project.py
@@ -1,3 +1,10 @@
+"""
+Student Name: Ramazan Burak Saritas, Ali Alperen Sonmez
+Student Number: 2020400321, 2020400354
+Compile Status: Compiling
+Program Status: Working
+Notes: 
+"""
 import argparse
 from mpi4py import MPI
 import os
@@ -45,45 +52,90 @@ def distribute_data():
 
 # Functions used in merging data for Requirement 2 and 3.
 def merge_data_master(calculated_data):
-    collected_data = []
+    collected_data = {}
+    # Workers sends their calculated data dictionary to Master.
     if (rank!=0):
         comm.send(calculated_data, dest=0, tag=rank)
+    # Master receives all workers data sequentially and adds to its data dictionary.
     else:
         number_of_workers = rank_count - 1
         # Each item in calculated_data array corresponds to total counts.
         for i in range(number_of_workers):
             received_data = comm.recv(source=i+1, tag=i+1)
-            for j in range(received_data):
-                collected_data[j] += received_data[j]
-    
+            for key in received_data:
+                if key in collected_data:
+                    collected_data[key]+=received_data[key]
+                else:
+                    collected_data[key]=received_data[key]
     return collected_data
 
 def merge_data_workers(calculated_data):
     number_of_workers = rank_count - 1
     last_worker = number_of_workers - 1
-    collected_data = []
-    if rank(!=0):
-        for i in range(number_of_workers):
-            previous_worker = rank-1
-            next_worker = rank+1
-            # Receives data from previous worker. First worker does not.
-            if (rank>1):
-                received_data = comm.recv(source=previous_worker, tag=previous_worker)
-                # Add received data to calculated data.
-                for j in range(received_data):
-                    calculated_data[j] += received_data[j] 
-            # Sends data to next worker. Last worker sends data to master.
-            if (rank!=last_worker):
-                comm.send(calculated_data, dest=next_worker, tag=rank)
-            else:
-                comm.send(calculated_data, dest=0, tag=rank)
+    collected_data = {}
+    # Workers receive data from the previous worker and send to the next.
+    if (rank!=0):
+        previous_worker = rank-1
+        next_worker = rank+1
+        # Receives data from previous worker. First worker does not.
+        if (rank>1):
+            received_data = comm.recv(source=previous_worker, tag=previous_worker)
+            # Add received data to calculated data.
+            for key in received_data:
+                if key in collected_data:
+                    collected_data[key]+=received_data[key]
+                else:
+                    collected_data[key]=received_data[key]
+        # Workers sends data to next worker.
+        if (rank!=last_worker):
+            comm.send(calculated_data, dest=next_worker, tag=rank)
+        # Last worker sends to the Master.
+        else:
+            comm.send(calculated_data, dest=0, tag=rank)
+    # Master receives data from last worker.
     else:
         collected_data = comm.recv(source=last_worker, tag=last_worker)
-    
     return collected_data
 
-# TODO: Functions used in counting unigrams and bigrams.
-# TODO: Functions used in computing the conditional probabilites of bigrams.
+
+#compute_unigrams_bigrams function computes frequincies of bigrams and unigrams, and records them in a dictionary.
+def compute_unigrams_bigrams(data):
+    #increment_value function checks if a key exist in a dict. If so, increments value. Else, adds key to dict with value=1.
+    def increment_value(key,dct):
+        if key in dct:
+            dct[key] += 1
+        else:
+            dct[key] = 1
+
+    dct = dict()
+    for sentence in data:
+        if len(sentence)==0:
+            continue
+        if len(sentence)==1:
+            increment_value(sentence[0], dct)
+            continue
+
+        increment_value(sentence[0], dct)
+        for idx in range(len(sentence)-1):
+            bigram = sentence[idx] + " " + sentence[idx+1]
+            unigram = sentence[idx+1]
+            increment_value(bigram, dct)
+            increment_value(unigram, dct)  
+    return dct
+               
+# Takes "new technologies" and dict as arguments and returns P(technologies|new) = Freq(new technologies)/Freq(new)
+def compute_conditional_probability(unigram_bigram_count, bigram, unigram):
+    count_bigram = 0
+    count_unigram = 0
+    if bigram in unigram_bigram_count:
+        count_bigram = unigram_bigram_count[bigram]
+    if unigram in unigram_bigram_count:
+        count_unigram = unigram_bigram_count[unigram]
+    return count_bigram/count_unigram
+
+merge_method = ""
+test_file = ""
+unigram_bigram_count = {}
 
 # Requirement 1
 if (rank==0):
@@ -92,27 +144,40 @@ if (rank==0):
     parser.add_argument("-merge_method", "--merge_method", dest = "merge_method")
     parser.add_argument("-test_file", "--test_file", dest = "test_file")
     args = parser.parse_args()
+    merge_method = args.merge_method
+    test_file = args.test_file
+    # Send data to workers of the merge method.
+    number_of_workers = rank_count - 1
+    for i in range(number_of_workers):
+        worker_rank = i+1
+        comm.send(merge_method, dest=worker_rank, tag=0)
+    # Read input file.
     read_file(args.input_file)
+    # Distribute the lists of sentences to workers.
     distribute_data()
 
 # Requirement 2
-calculated_data = []
 else:
+    merge_method = comm.recv(source=0, tag=0)
     data = comm.recv(source=0, tag=rank)
     print("Rank {} received {} sentences.".format(rank, len(data)))
     # TODO: count unigrams and bigrams
-    # TODO: add calculated data to array calculated_data to send it later
-    # TODO: modify calculated data defined above
-
-collected_data = []
-if (args.merge_method == "MASTER"):
-    collected_data = merge_data_master(calculated_data)
+    dct = compute_unigrams_bigrams(data)
+    unigram_bigram_count = dct
+    # TODO: add calculated data to array calculated_data to send it later 
 
 # Requirement 3
-elif (args.merge_method == "WORKERS")
-    collected_data = merge_data_workers(calculated_data)
+if(merge_method=="MASTER"):
+    unigram_bigram_count = merge_data_master(unigram_bigram_count)
 
-# Requirement 4
+elif(merge_method=="WORKERS"):
+    unigram_bigram_count = merge_data_workers(unigram_bigram_count)
+
+# Requirement 4
 if (rank==0):
-    # TODO: compute conditional probabilites of bigrams read from input file.
-    # TODO: print bigrams and their conditional probabilities.
+    bigram_file = open(test_file, "r")
+    for bigram in bigram_file:
+        unigram = bigram.split()[0]
+        conditional_probability = compute_conditional_probability(unigram_bigram_count, bigram.rstrip(), unigram)
+        print(bigram.rstrip(), ":", conditional_probability)
+    bigram_file.close()
